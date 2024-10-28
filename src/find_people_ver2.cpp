5b06e3f6ec9b03897addfac007504152d9fdae3b
YamaguchiAtsushi <yamaguchi.atsushi2000@gmail.com>
2024-10-28 14:49:13 +0900

    add tracked_people and detected_people

--- src/find_people_ver2.cpp
+++ src/find_people_ver2.cpp
@@ -15,17 +15,19 @@ struct Person
     double aspect_ratio;                         // アスペクト比
     geometry_msgs::Point min_point;             // バウンディングボックスの最小点
     geometry_msgs::Point max_point;             // バウンディングボックスの最大点
-    int id;
+    bool is_matched;
 };
 
 class LidarClustering
 {
 public:
-    LidarClustering() : next_id_(0)
+    LidarClustering()
     {
         scan_sub_ = nh_.subscribe("/scan", 10, &LidarClustering::scanCallback, this);
         cluster_pub_ = nh_.advertise<visualization_msgs::MarkerArray>("lidar_clusters", 10);
-        people_pub_ = nh_.advertise<visualization_msgs::MarkerArray>("detected_people", 10);
+        detected_people_pub_ = nh_.advertise<visualization_msgs::MarkerArray>("detected_people", 10);
+        tracked_people_pub_ = nh_.advertise<visualization_msgs::MarkerArray>("tracked_people", 10);
+
     }
 
     void scanCallback(const sensor_msgs::LaserScan::ConstPtr& scan)
@@ -65,7 +67,7 @@ public:
             clusters.push_back(current_cluster);
         }
 
-        // publishClusters(clusters);
+        publishClusters(clusters);
         detectPeople(clusters);
     }
 
@@ -73,13 +75,9 @@ private:
     ros::NodeHandle nh_;
     ros::Subscriber scan_sub_;
     ros::Publisher cluster_pub_;
-    ros::Publisher people_pub_;
+    ros::Publisher detected_people_pub_;
+    ros::Publisher tracked_people_pub_;
 
-    std::vector<Person> tracked_people_;
-    std::vector<Person> detected_people_;
-    std::vector<Person> lost_people_;
-
-    int next_id_;
 
     double distance(const geometry_msgs::Point& p1, const geometry_msgs::Point& p2)
     {
@@ -139,7 +137,9 @@ private:
 
     void detectPeople(const std::vector<std::vector<geometry_msgs::Point>>& clusters)
     {
-        visualization_msgs::MarkerArray people_markers;
+        visualization_msgs::MarkerArray detected_people_markers;
+        visualization_msgs::MarkerArray tracked_people_markers;
+
         int marker_id = 0;
 
         // 人の検出結果を管理するためのリスト
@@ -147,6 +147,7 @@ private:
 
         for (const auto& cluster : clusters)
         {
+            
             Person person;
             person.points = cluster;
             person.average_distance = calculateAverageDistance(cluster);
@@ -160,72 +161,93 @@ private:
             if (isValidPerson(person))
             {
                 detected_people.push_back(person);
-                publishPersonMarker(people_markers, person, marker_id++);
+                publishPersonMarker(detected_people_markers, person, marker_id++, 0.0, 0.0, 1.0);
             }
+
+            trackPeople(detected_people);//場所はここでいいのか？？場所を変える場合、personの定義が必要
+            publishPersonMarker(tracked_people_markers, person, marker_id++, 1.0, 0.0, 0.0);
+            // std::vector<Person> tracked_people;
+
+            // for(const auto& tracked_person : tracked_people){
+            //     tracked_clusters.push_back(tracked_people.points);
+            // }
+            // publishClusters(tracked_clusters);
+
         }
 
-        updateTrackedPeople(detected_people);
+        // detected_people_pub_.publish(detected_people_markers);
+        tracked_people_pub_.publish(tracked_people_markers);
 
-        // for (size_t i = 0; i < tracked_people_.size(); ++i)
-        // {
-        //     publishPersonMarker(people_markers, tracked_people_[i], tracked_people_[i].id);
-        // }
-
-        people_pub_.publish(people_markers);
     }
 
-    void updateTrackedPeople(std::vector<Person>& detected_people_)
-    {
-        for(auto& detected_person : detected_people_)
-        {
-            double min_distance = std::numeric_limits<double>::max();
-            int best_match_index = -1;
+    void trackPeople(std::vector<Person> detected_people){
+        double people_movement_min;
+        double people_movement;
+        int matching_people_number = -1;
 
-            for(size_t i = 0; i < tracked_people_.size(); ++i)
-            {
-                geometry_msgs::Point tracked_people_xy = calculateAveragePoint(tracked_people_[i]);
-                geometry_msgs::Point detected_person_xy = calculateAveragePoint(detected_person);
-                double dist = distance(tracked_people_xy, detected_person_xy);
+        std::vector<Person> tracked_people;
+        if(!tracked_people.empty()){
+            for(size_t i = 0; i < detected_people.size(); i++){
+                detected_people[i].is_matched = false;
+                people_movement_min = 10000;
+                matching_people_number = -1;
 
-                if (dist < min_distance)
-                {
-                    min_distance = dist;
-                    best_match_index = i;
+                for(size_t j = 0; j < tracked_people.size(); j++){
+                    people_movement = distance(calcAveragePoint(detected_people[i].points), calcAveragePoint(tracked_people[j].points));
+                    if(people_movement < 0.5){
+                        if(people_movement_min > people_movement){
+                            people_movement_min = people_movement;
+                            matching_people_number = j;
+                        }
+                    }
+                }
+
+                if(matching_people_number != -1){//すべてのマッチング探索後に、最適なマッチングをする
+                    tracked_people[matching_people_number] = detected_people[i];
+                    tracked_people[matching_people_number].is_matched = true;
+                    detected_people[i].is_matched = true;//重複マッチングの防止
                 }
             }
 
-            if (best_match_index != -1)
-            {
-                tracked_people_[best_match_index] = detected_person;  // 更新
+            // is_matched が false の要素を削除
+            for (size_t j = 0; j < tracked_people.size(); ) {
+                if (!tracked_people[j].is_matched) {
+                    tracked_people.erase(tracked_people.begin() + j);
+                } else {
+                    j++; // 削除しない場合はインデックスを進める
+                }
             }
-            else
-            {
-                detected_person.id = next_id_++;
-                tracked_people_.push_back(detected_person);  // 新規追加
+        } else {
+            // tracked_people が空の場合、初期化
+            for (const auto& person : detected_people) {
+                tracked_people.push_back(person);
             }
         }
+        
+
+        std::cout << "detected_people.size():" << detected_people.size() << std::endl;
+        std::cout << "tracked_people.size():" << tracked_people.size() << std::endl << std::endl;
+
     }
 
-    geometry_msgs::Point calculateAveragePoint(const Person& person)
-    {
-        geometry_msgs::Point average_point;
-        average_point.x = 0.0;
-        average_point.y = 0.0;
-        average_point.z = 0.0;
+    geometry_msgs::Point calcAveragePoint(const std::vector<geometry_msgs::Point>& cluster){
+        double sum_cluster_point_x = 0.0;
+        double sum_cluster_point_y = 0.0;
+        double ave_cluster_point_x = 0.0;
+        double ave_cluster_point_y = 0.0;
 
-        for (const auto& point : person.points)
-        {
-            average_point.x += point.x;
-            average_point.y += point.y;
+        for(size_t i = 0; i < cluster.size(); i++){
+             sum_cluster_point_x +=cluster[i].x;
+             sum_cluster_point_y +=cluster[i].y;
         }
+        ave_cluster_point_x = sum_cluster_point_x / cluster.size();
+        ave_cluster_point_y = sum_cluster_point_y / cluster.size();
 
-        if (!person.points.empty())
-        {
-            average_point.x /= person.points.size();
-            average_point.y /= person.points.size();
-        }
+        geometry_msgs::Point ave_cluster_point;
+        ave_cluster_point.x = ave_cluster_point_x;
+        ave_cluster_point.y = ave_cluster_point_y;
 
-        return average_point;
+        return ave_cluster_point;
     }
 
     double calculateLength(const geometry_msgs::Point& min_point, const geometry_msgs::Point& max_point)
@@ -242,90 +264,109 @@ private:
 
     bool isValidPerson(const Person& person)
     {
+        // フィルタリング条件をここに記述
         double avg_distance = person.average_distance;
         int point_count = person.points.size();
+        
+        // min_point と max_point を使用して長さとアスペクト比を計算
         double length = calculateLength(person.min_point, person.max_point);
         double aspect_ratio = calculateAspectRatio(length, person.min_point, person.max_point);
 
-        return (   (avg_distance <= 5.0 && point_count >= 15 && point_count <= 70 && length >= 0.25 && length <= 1.5)
-                && (aspect_ratio >= 0.5 && aspect_ratio <= 3.0));
+        return (   (avg_distance <= 5.0 && point_count >= 15 && point_count <= 70 && length >= 0.25 && length <= 0.7 && aspect_ratio >= 1.0 && aspect_ratio <= 3.0) 
+                || (avg_distance > 5.0 && avg_distance <= 10.0 && point_count >= 10 && point_count <= 35 && length >= 0.25 && length <= 0.65 && aspect_ratio >= 1.0 && aspect_ratio <= 3.0)
+                || (avg_distance > 10.0 && point_count >= 5 && point_count <= 18 && length >= 0.25 && length <= 0.7 && aspect_ratio >= 1.0 && aspect_ratio <= 3.0)
+            );
     }
 
-    double calculateAverageDistance(const std::vector<geometry_msgs::Point>& cluster)
+    void publishPersonMarker(visualization_msgs::MarkerArray& people_markers, const Person& person, int marker_id, int red_color, int green_color, int blue_color)
     {
-        double total_distance = 0.0;
-        for (const auto& point : cluster)
-        {
-            total_distance += sqrt(point.x * point.x + point.y * point.y);
-        }
-        return total_distance / cluster.size();
+        visualization_msgs::Marker bbox_marker;
+        bbox_marker.header.frame_id = "laser";
+        bbox_marker.header.stamp = ros::Time::now();
+        bbox_marker.ns = "detected_people";
+        bbox_marker.id = marker_id;
+        bbox_marker.type = visualization_msgs::Marker::LINE_LIST;
+        bbox_marker.action = visualization_msgs::Marker::ADD;
+        bbox_marker.scale.x = 0.05;
+        bbox_marker.color.r = red_color;
+        bbox_marker.color.g = green_color;
+        bbox_marker.color.b = blue_color;
+        // bbox_marker.color.r = 1.0;
+        // bbox_marker.color.g = 0.0;
+        // bbox_marker.color.b = 0.0;
+        bbox_marker.color.a = 1.0;
+
+        geometry_msgs::Point p1;
+        p1.x = person.min_point.x;
+        p1.y = person.min_point.y;
+        p1.z = 0.0;
+
+        geometry_msgs::Point p2;
+        p2.x = person.max_point.x;
+        p2.y = person.min_point.y;
+        p2.z = 0.0;
+
+        geometry_msgs::Point p3;
+        p3.x = person.max_point.x;
+        p3.y = person.max_point.y;
+        p3.z = 0.0;
+
+        geometry_msgs::Point p4;
+        p4.x = person.min_point.x;
+        p4.y = person.max_point.y;
+        p4.z = 0.0;
+
+        bbox_marker.points.push_back(p1);
+        bbox_marker.points.push_back(p2);
+        bbox_marker.points.push_back(p2);
+        bbox_marker.points.push_back(p3);
+        bbox_marker.points.push_back(p3);
+        bbox_marker.points.push_back(p4);
+        bbox_marker.points.push_back(p4);
+        bbox_marker.points.push_back(p1);
+
+        people_markers.markers.push_back(bbox_marker);
     }
 
     std::pair<geometry_msgs::Point, geometry_msgs::Point> calculateBoundingBox(const std::vector<geometry_msgs::Point>& cluster)
     {
-        geometry_msgs::Point min_point;
-        geometry_msgs::Point max_point;
-        min_point.x = min_point.y = std::numeric_limits<double>::max();
-        max_point.x = max_point.y = std::numeric_limits<double>::lowest();
+        geometry_msgs::Point min_point, max_point;
+        min_point.x = std::numeric_limits<double>::max();
+        min_point.y = std::numeric_limits<double>::max();
+        max_point.x = std::numeric_limits<double>::lowest();
+        max_point.y = std::numeric_limits<double>::lowest();
 
         for (const auto& point : cluster)
         {
-            if (point.x < min_point.x) min_point.x = point.x;
-            if (point.y < min_point.y) min_point.y = point.y;
-            if (point.x > max_point.x) max_point.x = point.x;
-            if (point.y > max_point.y) max_point.y = point.y;
+            min_point.x = std::min(min_point.x, point.x);
+            min_point.y = std::min(min_point.y, point.y);
+            max_point.x = std::max(max_point.x, point.x);
+            max_point.y = std::max(max_point.y, point.y);
         }
 
         return {min_point, max_point};
     }
 
-    void publishPersonMarker(visualization_msgs::MarkerArray& markers, const Person& person, int id)
+    double calculateAverageDistance(const std::vector<geometry_msgs::Point>& cluster)
     {
-        visualization_msgs::Marker marker;
-        marker.header.frame_id = "laser";
-        marker.header.stamp = ros::Time::now();
-        marker.ns = "people";
-        marker.id = id;
-        marker.type = visualization_msgs::Marker::CUBE;
-        marker.action = visualization_msgs::Marker::ADD;
-        marker.pose.position.x = (person.min_point.x + person.max_point.x) / 2.0;
-        marker.pose.position.y = (person.min_point.y + person.max_point.y) / 2.0;
-        marker.pose.position.z = 0.0; // 高さを 0 に設定
-        marker.scale.x = person.length;
-        marker.scale.y = person.length * person.aspect_ratio; // アスペクト比を使用
-        marker.scale.z = 0.2; // 高さを 0.2 に設定
-        marker.color.r = 0.0;
-        marker.color.g = 0.0;
-        marker.color.b = 1.0;
-        marker.color.a = 0.5;
+        double total_distance = 0.0;
 
-        markers.markers.push_back(marker);
+        for (const auto& point : cluster)
+        {
+            geometry_msgs::Point origin;
+            origin.x = 0.0;
+            origin.y = 0.0;
+            origin.z = 0.0;
+            total_distance += distance(origin, point); // 原点からの距離
+        }
 
-        // ID を表示するためのテキストマーカを追加
-        visualization_msgs::Marker text_marker;
-        text_marker.header.frame_id = "laser";
-        text_marker.header.stamp = ros::Time::now();
-        text_marker.ns = "people_text";
-        text_marker.id = id + 1000;  // 一意なIDを使うためのオフセット
-        text_marker.type = visualization_msgs::Marker::TEXT_VIEW_FACING;
-        text_marker.action = visualization_msgs::Marker::ADD;
-        text_marker.pose.position.x = marker.pose.position.x;
-        text_marker.pose.position.y = marker.pose.position.y;
-        text_marker.pose.position.z = marker.pose.position.z + 0.2; // テキストを上に表示
-        text_marker.scale.z = 0.2;
-        text_marker.color.r = 1.0;
-        text_marker.color.g = 1.0;
-        text_marker.color.b = 1.0;
-        text_marker.color.a = 1.0;
-        text_marker.text = "ID: " + std::to_string(person.id);
-
-        markers.markers.push_back(text_marker);
+        return total_distance / cluster.size();
     }
 };
 
 int main(int argc, char** argv)
 {
-    ros::init(argc, argv, "lidar_clustering");
+    ros::init(argc, argv, "lidar_clustering_node");
     LidarClustering lc;
     ros::spin();
     return 0;
